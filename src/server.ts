c06c41d3d0179dc9787d3176ad92600250d1e08a
jofr <11932850-jfromgallifrey@users.noreply.gitlab.com>
2022-10-10 22:59:45 +0200

    feat: turn credentials only for allowed origins
    only provide rest api turn credentials if the request is a cors request
    coming from one of the allowed origins

--- src/server.ts
+++ src/server.ts
@@ -10,37 +10,54 @@ const argv = yargs
     .default("key", "")
     .default("port", "")
     .default("turn-secret", "")
+    .default("allowed-turn-origins", [])
     .config()
     .argv
 
 let server;
 
+function getTurnCredentials(req, res) {
+    const origin = req.headers.origin;
+    if (!argv.allowedTurnOrigins.includes(origin)) {
+        res.writeHead(403);
+        res.end();
+        return;
+    }
+
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+
+    if (req.method === "OPTIONS") {
+        res.writeHead(200);
+        res.end();
+    } else {
+        res.setHeader("Content-Type", "application/json");
+        
+        const ttl = 86400; // seconds (so 24 hours)
+        const expirationTimestamp = Math.floor(Date.now()/1000) + ttl;
+        const username = `${expirationTimestamp}:listentogether`;
+
+        const hmac = crypto.createHmac("sha1", argv.turnSecret);
+        hmac.setEncoding("base64");
+        hmac.write(username);
+        hmac.end()
+        const password = hmac.read();
+
+        res.writeHead(200);
+        res.end(JSON.stringify({
+            username: username,
+            password: password,
+            ttl: ttl
+        }));
+    }
+}
+
 function serverRequest(req, res) {
-    switch (req.url) {
-        case "/turn_credentials":
-            res.setHeader("Content-Type", "application/json");
-            res.writeHead(200);
-
-            const ttl = 86400; // seconds (so 24 hours)
-            const expirationTimestamp = Math.floor(Date.now()/1000) + ttl;
-            const username = `${expirationTimestamp}:listentogether`;
-
-            const hmac = crypto.createHmac("sha1", argv.turnSecret);
-            hmac.setEncoding("base64");
-            hmac.write(username);
-            hmac.end()
-            const password = hmac.read();
-
-            res.end(JSON.stringify({
-                username: username,
-                password: password,
-                ttl: ttl
-            }));
-
-            break;
-        default:
-            res.writeHead(404);
-            res.end();
+    if (req.url === "/turn_credentials") {
+        getTurnCredentials(req, res);
+    } else {
+        res.writeHead(404);
+        res.end();
     }
 }
 
